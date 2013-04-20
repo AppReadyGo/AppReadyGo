22910b52cb4cbeb9debc3893d21170bfe6c52dd2
Yura <Yura@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2013-04-20 17:10:06 +0000

    Remove tables and fix mapping

--- AppReadyGo/Database/Schemas/api/Roles.table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [api].[Roles] (
-    [ID] INT NOT NULL,
-    [Name] NVARCHAR(50) NOT NULL
-);
-GO;
-
-ALTER TABLE [api].[Roles]
-ADD PRIMARY KEY CLUSTERED ([ID] ASC);
-GO;
---
+++ AppReadyGo/Domain/Mapping/ApplicationTypeMapping.cs
@@ -0,0 +1,18 @@
+﻿using AppReadyGo.Domain.Model;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace AppReadyGo.Domain.Mapping
+{
+    public class ApplicationTypeMapping : ClassMapping<ApplicationType>
+    {
+        public ApplicationTypeMapping()
+        {
+            Table("ApplicationTypes");
+
+            Id(x => x.Id, map => { map.Generator(Generators.Identity); map.Column("ID"); });
+            Property(x => x.Name, map => { map.NotNullable(true); map.Length(70); });
+        }
+    }
+}
+
