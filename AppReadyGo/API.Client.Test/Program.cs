1f2235e86783e3f0c1d7b4ca1ff16c5df11cfbb5
Pavel <Pavel@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2013-04-23 15:52:06 +0000

    API test added

---
+++ AppReadyGo/API.Client.Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using AppReadyGo.API.Models;
+
+namespace API.Client.Test
+{
+    class Program
+    {
+
+        static readonly Uri _baseAddress = new Uri("http://localhost:63321/");
+
+        static void Main(string[] args)
+        {
+
+            HttpClient client = new HttpClient();
+
+            var user = new { id = "user1", pass = "1234" };
+
+            // Post contact
+            Uri address = new Uri(_baseAddress, "/api/readygomarket/");
+
+            //HttpResponseMessage response =  await client.GetAsync(address);
+
+            // Check that response was successful or throw exception
+            //response.EnsureSuccessStatusCode();
+
+
+
+        }
+    }
+}
