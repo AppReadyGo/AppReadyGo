0d5864f91e214bdca9c8b6beb22b10b1ae19e27a
Yura <Yura@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2013-04-24 06:21:48 +0000

    Add comments

--- AppReadyGo/API/Controllers/ReadyGoMarketController.cs
+++ AppReadyGo/API/Controllers/ReadyGoMarketController.cs
@@ -10,16 +10,19 @@ namespace AppReadyGo.API.Controllers
 {
     public class ReadyGoMarketController : ApiController
     {
-
+        // TODO: Yura: Why do we need the repositories? We already have DAL, this is just add additional layer that we don't need
+        // In addition the approach to init objects, prevent test creating.
         static readonly IMarketAppsRepository mAppsRepository = new MarketAppsRepository();
         static readonly IMarketTestersRepository mTestersRepository = new MarketTestersRepository();
 
+        // TODO: Yura: the id in string format, is it MVC.API restrictions?
         public bool GetLogin(string id, string pass)
         {
             return false;
         }
 
-        
+
+        // TODO: Yura: We do not need the last parameter, you will not call for the method if user did not accept terms and conditions.
         public bool GetRegister(string name, string email, string pass, string gender, string contry, string zip, int[] interest, bool terms)
         {
             return true;
