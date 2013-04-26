e1a6178276f2d0b54e3c3b9cba06e8e129bed7a8
Pavel <Pavel@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2013-04-26 06:22:39 +0000

    rename controller

---
+++ AppReadyGo/API/Controllers/MarketController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AppReadyGo.API.Models;
+
+namespace AppReadyGo.API.Controllers
+{
+    public class MarketController : ApiController
+    {
+        // TODO: Yura: Why do we need the repositories? We already have DAL, this is just add additional layer that we don't need
+        // In addition the approach to init objects, prevent test creating.
+        static readonly IMarketAppsRepository mAppsRepository = new MarketAppsRepository();
+        static readonly IMarketTestersRepository mTestersRepository = new MarketTestersRepository();
+
+        [HttpPost]
+        public bool Login(string id, string pass)
+        {
+            return false;
+        }
+
+
+        // TODO: Yura: We do not need the last parameter, you will not call for the method if user did not accept terms and conditions.
+        [HttpPost]
+        public bool Register(string name, string email, string pass, string gender, string contry, string zip, int[] interest, bool terms)
+        {
+            return true;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Application> GetApps(string id)
+        {
+            return null;
+        }
+
+
+
+    }
+}
