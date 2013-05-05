81a3bb42d02fedc5a055ff3050338d2b79e4ef9a
Yura <ypanshin@users.noreply.github.com>
2013-05-05 15:00:15 +0000

    Add test

--- API/Global.asax.cs
+++ API/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppReadyGo.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,6 +23,8 @@ namespace AppReadyGo.API
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ObjectContainer.Instance.GetType();
         }
     }
 }
