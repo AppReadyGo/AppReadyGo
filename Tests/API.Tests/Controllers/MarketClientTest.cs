abcfcac5089874888e559e081005acad4fac3729
Yura <ypanshin@users.noreply.github.com>
2013-05-14 07:40:12 +0000

    add new test

--- Tests/API.Tests/Controllers/MarketClientTest.cs
+++ Tests/API.Tests/Controllers/MarketClientTest.cs
@@ -30,7 +30,7 @@ namespace AppReadyGo.API.Tests.Controllers
 
             var data = new LoginModel { Email = "test@test.com", Password = "1234" };
 
-            var response = client.PostAsJsonAsync("Login", data).Result;
+            var response = client.PostAsJsonAsync("login", data).Result;
             if (!response.IsSuccessStatusCode)
             {
                 var res = response.Content.ReadAsStringAsync();
@@ -41,6 +41,51 @@ namespace AppReadyGo.API.Tests.Controllers
                 var res = response.Content.ReadAsAsync<bool>();
                 Assert.IsTrue(res.Result);
             }
+        }
+
+        [TestMethod]
+        public void MarketRegisterByNetwork()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = _baseAddress;
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var data = new RegisterModel { ContryId = 1, Email = "test@test.com", Password = "1234", FirstName = "xxx" };
+
+            var response = client.PostAsJsonAsync("Register", data).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                var res = response.Content.ReadAsStringAsync();
+                Assert.Fail(string.Format("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase));
+            }
+            else
+            {
+                var res = response.Content.ReadAsAsync<bool>();
+                Assert.IsTrue(res.Result);
+            }
+        }
+
+        [TestMethod]
+        public void MarketRegisterByNetwork1()
+        {
+
+            System.Net.WebRequest req = System.Net.WebRequest.Create("http://localhost:63321/api/Market/Register");
+            //Add these, as we're doing a POST
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.Method = "POST";
+            //We need to count how many bytes we're sending. Post'ed Faked Forms should be name=value&
+            byte[] bytes = System.Text.Encoding.ASCII.GetBytes("{{\"email\":\"test@test.com\"}}");
+            req.ContentLength = bytes.Length;
+            System.IO.Stream os = req.GetRequestStream();
+            os.Write(bytes, 0, bytes.Length); //Push it out there
+            os.Close();
+
+            System.Net.WebResponse resp = req.GetResponse();
+            //if (resp == null) return null;
+            System.IO.StreamReader sr = new System.IO.StreamReader(resp.GetResponseStream());
+            var res = sr.ReadToEnd().Trim();
         }
 
         //[TestMethod]
