1684c9d4935eac1fd6de7973c0f169a72a549906
Yura <Yura@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2013-05-19 11:21:02 +0000

    Add tests

--- AppReadyGo/Domain/Model/AccountProfiler.cs
+++ AppReadyGo/Domain/Model/AccountProfiler.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using AppReadyGo.Domain.Model.BackOffice;
-using AppReadyGo.Domain.Model.Users;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using AppReadyGo.Domain.Model.BackOffice;
+//using AppReadyGo.Domain.Model.Users;
 
-namespace AppReadyGo.Domain.Model
-{
-    public class AccountProfiler
-    {
-        public virtual int Id { get; set; }
-        public virtual int UpdateFriquency { get; set; }
-        public virtual decimal Price { get; set; }
-        public virtual IList<User> Users { get; set; }
-    }
-}
+//namespace AppReadyGo.Domain.Model
+//{
+//    public class AccountProfiler
+//    {
+//        public virtual int Id { get; set; }
+//        public virtual int UpdateFriquency { get; set; }
+//        public virtual decimal Price { get; set; }
+//        public virtual IList<User> Users { get; set; }
+//    }
+//}
--- AppReadyGo/Tests/Domain.Tests/ValidationContextTest.cs
+++ AppReadyGo/Tests/Domain.Tests/ValidationContextTest.cs
@@ -2,22 +2,65 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AppReadyGo.Domain.CommandHandlers;
 using AppReadyGo.Common;
+using NHibernate;
+using AppReadyGo.Domain.Model.Users;
 
 namespace Domain.Tests
 {
     [TestClass]
     public class ValidationContextTest
     {
+        private Database database;
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            this.database = new Database("QA", "arg_unittests");
+        }
+
+        [TestCleanup()]
+        public void TestCleanUp()
+        {
+            this.database.Clear();
+        }
+
         [TestMethod]
         public void IsEmailExistsForNewUser()
         {
-            // ObjectContainer.Instance.
-            // var validation = new ValidationContext(
+            int userId = CreateUser();
+            using (ISession session = database.OpenSession())
+            {
+                var validation = new ValidationContext(session);
+                Assert.IsTrue(validation.IsEmailExists("test@test.com"));
+                Assert.IsFalse(validation.IsEmailExists("test1@test.com"));
+            }
         }
 
         [TestMethod]
         public void IsEmailExistsForExistsUser()
         {
+            int userId = CreateUser("test1@test.com");
+            userId = CreateUser();
+            using (ISession session = database.OpenSession())
+            {
+                var validation = new ValidationContext(session);
+                Assert.IsFalse(validation.IsEmailExists("test@test.com", userId));
+                Assert.IsTrue(validation.IsEmailExists("test1@test.com", userId));
+            }
+        }
+
+        public int CreateUser(string email = "test@test.com")
+        {
+            var user = new Member(email, "12345");
+            using (ISession session = database.OpenSession())
+            {
+                using (ITransaction dbTrans = session.BeginTransaction())
+                {
+                    session.Save(user);
+                    dbTrans.Commit();
+                }
+            }
+            return user.Id;
         }
     }
 }
