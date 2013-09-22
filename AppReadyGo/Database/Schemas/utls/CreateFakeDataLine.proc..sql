aa9c1d3e1235f6113b6b4c0fef1297564ade7726
Philip <Philip@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2013-09-22 21:31:13 +0000

    Added eco schema, stored prodedures in utils

---
+++ AppReadyGo/Database/Schemas/utls/CreateFakeDataLine.proc..sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE [utls].[CreateFakeDataLine]
+	-- Add the parameters for the stored procedure here
+	@applicationId int,
+	@clientHeight int = 800,
+	@clientWidth int = 480,
+	@path nvarchar(256),
+	@x int,
+	@y int,
+	@offsetAroundThePoint int = 10,
+	@numberOfPointsToCreate int = 50000,
+	@interval int = 10,
+	@lastX int = 420
+AS
+
+
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	DECLARE @i INT
+	
+	WHILE (@x < @lastX)
+--	BEGIN
+	BEGIN   PRINT @x
+	EXEC	@i = [utls].[CreateFakeData]
+	    @applicationId = @applicationId ,
+		@clientHeight = @clientHeight,
+		@clientWidth = @clientWidth,
+		@path = @path,
+		@x = @x,
+		@y = @y ,
+		@offsetAroundThePoint = @offsetAroundThePoint,
+		@numberOfPointsToCreate = @numberOfPointsToCreate
+		
+	SET @x = @x + @interval
+	
+	END
+	print N'Created ' +  + convert(varchar(9), @i)
+END
+
---
+++ AppReadyGo/Database/Schemas/utls/CreateFakePageView.proc.sql
@@ -0,0 +1,92 @@
+﻿CREATE PROCEDURE [utls].[CreateFakePageView]
+	-- Add the parameters for the stored procedure here
+	@applicationId int,
+	@screenHeight int = 800,
+	@screenWidth int = 480,
+	@path nvarchar(256),
+	@x int,
+	@y int,
+	@offsetY  int = 10,
+	@offsetX  int = 0,
+	@numberOfViewPartsToCreate int = 100,
+	@numberOfRandomVPToCreate int = 30
+AS
+
+
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	DECLARE @i INT
+	DECLARE @pageViewId INT
+	DECLARE @vScreenHeight INT
+	DECLARE @vClientHeight INT
+	DECLARE @date date
+	DECLARE @rad int,@vX int ,@vY int
+	SET @i = 1
+	SET NOCOUNT ON;
+	
+	select @pageViewId = max (c.PageViewId) 
+	  from dbo.[ClickView] c
+	 where c.ApplicationId = @applicationId
+	   and c.ScreenWidth = @screenWidth
+	   and c.ScreenHeight = @screenHeight
+	   and c.Path = @path; 
+	print  @pageViewId
+	if @pageViewId > 0 
+	begin
+	    select @vScreenHeight = p.ScreenHeight, @vClientHeight = p.ClientHeight, @date = p.Date
+	      from PageViews p
+	     where p.Id = @pageViewId; 
+	     
+		WHILE (@i <=@numberOfViewPartsToCreate + @numberOfRandomVPToCreate)
+		BEGIN
+		
+		 	SET @i  = @i + 1
+			
+			if @i <= @numberOfViewPartsToCreate 	 
+			begin 	   
+				INSERT INTO [dbo].[ViewParts]
+					   ([X]
+					   ,[Y]
+					   ,[StartDate]
+					   ,[FinishDate]
+					   ,[Orientation]
+					   ,[PageViewId])
+				 VALUES
+					   (0
+					   ,0
+					   ,@date
+					   ,@date
+					   ,0
+					   ,@pageViewId)
+             end
+				
+				  if  @i >  @numberOfViewPartsToCreate 	 
+				  begin
+				     set @vX = @offsetX *RAND(); 
+		    
+	   				 set @vY = @offsetY* rand();
+				
+					 PRINT 'x =' +  convert(varchar(4), @vX) + ' y= ' + convert(varchar(4), @vY)
+					 
+					 INSERT INTO [dbo].[ViewParts]
+					   ([X]
+					   ,[Y]
+					   ,[StartDate]
+					   ,[FinishDate]
+					   ,[Orientation]
+					   ,[PageViewId])
+					 VALUES
+					   (@vX
+					   ,@vY
+					   ,@date
+					   ,@date
+					   ,0
+					   ,@pageViewId)
+        end
+	END 
+    end
+    else PRINT 'PageView was not found, nothing inserted'
+	print N'Created ' +  + convert(varchar(9), @i)
+END
+
