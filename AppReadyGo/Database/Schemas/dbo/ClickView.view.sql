881486958795f2fbb1347a82d3a9de4b3f1c7373
Philip <Philip@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2013-09-22 21:15:12 +0000

    Added ContolClick tale, changed name formatting, added views

---
+++ AppReadyGo/Database/Schemas/dbo/ClickView.view.sql
@@ -0,0 +1,19 @@
+﻿CREATE view [dbo].[ClickView]
+as
+select 
+pv.ApplicationId,
+c.PageViewId,
+a.Description,
+pv.path,
+pv.ScreenWidth,
+pv.ScreenHeight,
+pv.ClientHeight,
+pv.ClientWidth,
+c.X,
+c.Y,
+c.Date,
+c.Orientation
+from applications a
+join PageViews pv on pv.ApplicationId = a.id
+join clicks c on c.PageViewId = pv.id
+
---
+++ AppReadyGo/Database/Schemas/dbo/ScrollView.view.sql
@@ -0,0 +1,15 @@
+﻿CREATE view [dbo].[ScrollView]
+as
+select 
+pv.ApplicationId,
+s.PageViewId,
+a.Description,
+pv.path,
+pv.ScreenWidth,
+pv.ClientHeight,
+s.Id,
+s.FirstTouchId,
+s.LastTouchId
+from applications a
+join PageViews pv on pv.ApplicationId = a.id
+join scrolls s on s.PageViewId = pv.id
