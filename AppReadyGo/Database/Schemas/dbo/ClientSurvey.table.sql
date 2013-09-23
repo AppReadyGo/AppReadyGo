1d1e82b284d10e086df09310069ca60483ffd9dd
Philip <Philip@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2013-09-23 13:51:03 +0000

    added names to Primary keys + another set of tables

---
+++ AppReadyGo/Database/Schemas/dbo/ClientSurvey.table.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[ClientSurvey](
+	[ClientSurveyId] [int] NOT NULL,
+	[UserId] [int] NOT NULL,
+	[SurveyId] [int] NOT NULL,
+CONSTRAINT [PK_ClientSurvey] PRIMARY KEY CLUSTERED 
+(
+	[ClientSurveyId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) 
+
+GO
+
+ALTER TABLE [dbo].[ClientSurvey]  ADD  CONSTRAINT [FK_ClientSurvey_Survey] FOREIGN KEY([SurveyId])
+REFERENCES [dbo].[Survey] ([SurveyId])
+GO
+
+ALTER TABLE [dbo].[ClientSurvey] CHECK CONSTRAINT [FK_ClientSurvey_Survey]
+GO
+
+ALTER TABLE [dbo].[ClientSurvey]  ADD  CONSTRAINT [FK_ClientSurvey_Users] FOREIGN KEY([UserId])
+REFERENCES [usr].[Users] ([ID])
+GO
+
+ALTER TABLE [dbo].[ClientSurvey] CHECK CONSTRAINT [FK_ClientSurvey_Users]
+GO
