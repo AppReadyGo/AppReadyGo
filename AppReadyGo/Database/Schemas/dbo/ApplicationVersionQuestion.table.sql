35790e526ba8b648472c89bb85b560f711e34196
Philip <Philip@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2013-09-23 13:57:45 +0000

    fixed ApplicationVersionQuestion

--- AppReadyGo/Database/Schemas/dbo/ApplicationVersionQuestion.table.sql
+++ AppReadyGo/Database/Schemas/dbo/ApplicationVersionQuestion.table.sql
@@ -4,11 +4,11 @@ CREATE TABLE [dbo].[ApplicationVersionQuestion](
 	[ApplicationId] [int] NOT NULL,
 	[VersionId] [int] NOT NULL,
 	[SurveyQuestionId] [int] NOT NULL,
-PRIMARY KEY CLUSTERED 
+CONSTRAINT [PK_ApplicationVersionQuestion]  PRIMARY KEY CLUSTERED 
 (
 	[ApplicationVersionQuestionyId] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-) ON [PRIMARY]
+) 
 
 GO
 
