59750f22c40dd8a033b7fd252defec62db25ff2f
Yura <Yura@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2013-09-29 15:01:51 +0000

    Add tests

---
+++ AppReadyGo/Common.Tests/Global.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Tests
+{
+    public static class Global
+    {
+#if QA
+        public static readonly Uri BaseAddress = new Uri("http://qa.appreadygo.com/");
+        public static readonly Uri ApiBaseAddress = new Uri("http://api.qa.appreadygo.com/");
+#elif DEBUG
+        public static readonly Uri BaseAddress = new Uri("http://localhost:63224/");
+        public static readonly Uri ApiBaseAddress = new Uri("");
+#else
+        public static readonly Uri BaseAddress = new Uri("http://appreadygo.com/");
+        public static readonly Uri ApiBaseAddress = new Uri("http://api.appreadygo.com/");
+#endif
+    }
+}
