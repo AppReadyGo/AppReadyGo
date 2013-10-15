7f90aff07d3d8e193d142964fcecfef42da6ed84
Yura <Yura@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2013-10-15 19:55:19 +0000

    add all applications query

--- AppReadyGo/Domain/QueriesHandlers/Application/GetAllApplicationsQueryHandler.cs
+++ AppReadyGo/Domain/QueriesHandlers/Application/GetAllApplicationsQueryHandler.cs
@@ -15,10 +15,8 @@ using AppReadyGo.Domain.Model.Users;
 
 namespace AppReadyGo.Domain.Queries.Application
 {
-    public class GetAllApplicationsQueryHandler : IQueryHandler<GetAllApplicationsQuery, ApplicationsDataResult>
+    public class GetAllApplicationsQueryHandler : IQueryHandler<GetAllApplicationsQuery, IEnumerable<ApplicationResult>>
     {
-        private static readonly ApplicationLogging log = new ApplicationLogging(MethodBase.GetCurrentMethod().DeclaringType);
-
         private ISecurityContext securityContext;
 
         public GetAllApplicationsQueryHandler(ISecurityContext securityContext)
@@ -26,132 +24,16 @@ namespace AppReadyGo.Domain.Queries.Application
             this.securityContext = securityContext;
         }
 
-        public ApplicationsDataResult Run(ISession session, GetAllApplicationsQuery query)
+        public IEnumerable<ApplicationResult> Run(ISession session, GetAllApplicationsQuery query)
         {
-            log.WriteInformation("-> Get all applications for SearchStr:{0}, PageSize:{1}, CurPage:{2}, User:{3}", query.SearchStr, query.PageSize, query.CurPage, securityContext.CurrentUser.Email);
-            var res = new ApplicationsDataResult();
-
-            var applicationsQuery = session.Query<Model.Application>()
-                        .Where(a => a.User.Id == securityContext.CurrentUser.Id);
-
-            if (!string.IsNullOrEmpty(query.SearchStr))
-            {
-                applicationsQuery = applicationsQuery.Where(a => a.Description.ToLower().Contains(query.SearchStr.ToLower()));
-            }
-
-            res.Count = applicationsQuery.Count();
-            res.TotalPages = (res.Count + query.PageSize - 1) / query.PageSize;
-            res.CurPage = query.CurPage > res.TotalPages ? res.TotalPages : query.CurPage;
-            res.PageSize = query.PageSize;
-
-            res.Applications = applicationsQuery.Select(a => new ApplicationDataItemResult
+            return session.Query<Model.Application>()
+                        .Where(a => a.User.Id == securityContext.CurrentUser.Id)
+                        .Select(a => new ApplicationResult
                                             {
                                                 Id = a.Id,
-                                                Name = a.Name,
-                                                Type = a.Type.Name,
-                                                Description = a.Description,
-                                                IconExt = a.IconExt,
-                                                HasScreens = a.Screens.Any()
+                                                Name = a.Name
                                             })
                                             .ToArray();
-
-            res.Applications = res.Applications.Skip(res.PageSize * (res.CurPage - 1))
-                                    .Take(res.PageSize)
-                                    .ToArray();
-            
-            //var visits = session.Query<PageView>()
-            //                    .Where(p => p.Application.User.Id == securityContext.CurrentUser.Id)
-            //                    .GroupBy(p => p.Application.Id)
-            //                    .Select(g => new
-            //                    {
-            //                        Key = g.Key,
-            //                        VisitsCount = g.Count(),
-            //                        LastRecivedDataDate = g.Max(x => x.Date)
-            //                    })
-            //                    .ToArray();
-
-            // Get top applications and top screens
-            var visitsByScreens = session.Query<PageView>()
-                    .Where(p => p.Application.User.Id == securityContext.CurrentUser.Id)
-                    .GroupBy(p => new
-                    {
-                        ApplicationId = p.Application.Id,
-                        Name = p.Application.Name,
-                        Path = p.Path,
-                        ScreenHeight = p.ScreenHeight,
-                        ScreenWidth = p.ScreenWidth
-                    })
-                    .Select(g => new
-                    {
-                        Key = g.Key,
-                        VisitsCount = g.Count(),
-                        LastRecivedDataDate = g.Max(x => x.Date)
-                    })
-                    .ToArray();
-
-            var visits = visitsByScreens.GroupBy(g => new
-                                {
-                                    ApplicationId = g.Key.ApplicationId,
-                                    Name = g.Key.Name,
-                                })
-                                .Select(g => new
-                                {
-                                    Key = g.Key,
-                                    VisitsCount = g.Count(),
-                                    LastRecivedDataDate = g.Max(x => x.LastRecivedDataDate)
-                                })
-                                .ToArray();
-
-            res.TopScreens = visitsByScreens.OrderByDescending(g => g.VisitsCount)
-                                            .Take(5)
-                                            .Select(g => new ApplicationScreenResult
-                                            {
-                                                ApplicationId = g.Key.ApplicationId,
-                                                Path = g.Key.Path,
-                                                ScreenSize = new Size(g.Key.ScreenWidth, g.Key.ScreenHeight)
-                                            }).ToArray();
-
-            res.TopApplications = visits.OrderByDescending(g => g.VisitsCount)
-                                            .Take(5)
-                                            .Select(g => new ApplicationResult
-                                            {
-                                                Id = g.Key.ApplicationId,
-                                                Name = g.Key.Name
-                                            }).ToArray();
-
-            var appIds = res.Applications.Select(x => x.Id).ToArray();
-            var appPublished = session.Query<Model.PublishDetails>()
-                        .Where(p => appIds.Contains(p.Application.Id))
-                        .Select(p => p.Application.Id)
-                        .ToArray();
-
-            var appDownloaded = session.Query<ApiMember>()
-                        .SelectMany(u => u.Applications)
-                        .Where(a => appIds.Contains(a.Id))
-                        .Select(a => a.Id)
-                        .GroupBy(id => id)
-                        .Select(x => new { ApplicationId = x.Key, Count = x.Count() })
-                        .ToArray();
-
-            // Aplicatyion is not active if was not recived data for 3 days
-            DateTime dt = DateTime.Now.AddDays(-3);
-
-            foreach (var application in res.Applications)
-            {
-                var count = visits.SingleOrDefault(a => a.Key.ApplicationId == application.Id);
-
-                application.Visits = count != null ? count.VisitsCount : 0;
-
-                application.IsActive = count != null && count.LastRecivedDataDate > dt ? true : false;
-
-                application.Published = appPublished.Contains(application.Id);
-
-                application.Downloaded = appDownloaded.Where(x => x.ApplicationId == application.Id).Select(x => x.Count).SingleOrDefault();
-
-            }
-            log.WriteInformation("Get all applications for portfolio ->");
-
-            return res;
         }
     }
 }
--- AppReadyGo/Web/Controllers/TaskController.cs
+++ AppReadyGo/Web/Controllers/TaskController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using AppReadyGo.Common;
 using AppReadyGo.Core.Queries.Analytics;
+using AppReadyGo.Core.Queries.Application;
 using AppReadyGo.Model.Pages.Application;
 
 namespace AppReadyGo.Web.Controllers
@@ -13,21 +14,8 @@ namespace AppReadyGo.Web.Controllers
     {
         public ActionResult New()
         {
-            var res = ObjectContainer.Instance.RunQuery(new TaskQuery(id));
-            var countries = res.Countries.Select(x => new SelectListItem { Text = x.Value, Value = x.Key.ToString() }).ToList();
-            countries.Insert(0, new SelectListItem { Text = "All", Value = "" });
-            var genders = new SelectListItem[] { new SelectListItem { Value = "", Text = "All" }, new SelectListItem { Value = "1", Text = "Men" }, new SelectListItem { Value = "2", Text = "Women" } };
-            var ageRanges = GetList<AgeRange>().Select(x => new SelectListItem { Value = ((int)x).ToString(), Text = x.ToString() }).ToList();
-            ageRanges.Insert(0, new SelectListItem { Text = "All", Value = "" });
-            var model = new TaskModel
-            {
-                ApplicationId = id,
-                ApplicationName = res.ApplicationName,
-                Countries = countries,
-                Genders = genders,
-                AgeRanges = ageRanges
-            };
-            return View("~/Views/Task/Edit.cshtml", model);
+            var res = ObjectContainer.Instance.RunQuery(new GetAllApplicationsQuery());
+            return View("~/Views/Task/Edit.cshtml", res);
         }
 
     }
